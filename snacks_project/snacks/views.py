b58803461f75eb17dd692fdafd32200ffb5e6d76
Ashrf Mathkour Obeidat <ashrfmathkour@gmail.com>
2021-11-14 15:21:21 +0200

    finished

--- snacks_project/snacks/views.py
+++ snacks_project/snacks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+
+
+
+## views defined here
+
+
+class HomePageView(TemplateView):
+    template_name='home.html'
+
+class AboutUsPageView(TemplateView):
+    template_name='about_us.html'
